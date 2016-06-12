f743c205c166acc19261b1d1e9c5c7e6cf9cc8cb
Doug Gilliland <doug@douglasgilliland.com>
2016-06-12 10:34:26 -0400

    trig macros

--- HSSP/HSSP_Programmer/Swd_PhysicalLayer.h
+++ HSSP/HSSP_Programmer/Swd_PhysicalLayer.h
@@ -51,7 +51,7 @@
 *                          PSoC 5LP (idle))
 ********************************************************************************/
 
-#define TRIG_DRIVEMODE_CMOSOUT         	pinMode(TRIG_Pin,OUTPUT)
+//#define TRIG_DRIVEMODE_CMOSOUT         	pinMode(TRIG_Pin,OUTPUT)
 
 #define SWDIO_DRIVEMODE_HIGHZIN         pinMode(SWDIO_Pin,INPUT)
 #define SWDIO_DRIVEMODE_CMOSOUT         pinMode(SWDIO_Pin,OUTPUT)
@@ -65,8 +65,8 @@
 /********* WiringPi Functions for driving output pins on host side **************
 ********************************************************************************/
 
-#define TRIG_OUTPUT_HIGH      digitalWrite(TRIG_Pin,1)
-#define TRIG_OUTPUT_LOW       digitalWrite(TRIG_Pin,0)
+//#define TRIG_OUTPUT_HIGH      digitalWrite(TRIG_Pin,1)
+//#define TRIG_OUTPUT_LOW       digitalWrite(TRIG_Pin,0)
 
 #define SWDIO_OUTPUT_HIGH      digitalWrite(SWDIO_Pin,1)
 #define SWDIO_OUTPUT_LOW       digitalWrite(SWDIO_Pin,0)
@@ -101,6 +101,8 @@ void SetXresHigh(void);
 void SetXresLow(void);
 void SetXresCmosOutput(void);
 void SetXresHizInput(void);
+
+void setupTrig(void);
 void trigger(void);
 
 #endif
